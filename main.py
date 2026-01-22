84cb337312a532694b664cf53f6ae45493d65259
blackcathanni <kikimnicole08@gmail.com>
2026-01-22 21:48:16 +0800

    string, integers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-firstName = "Minji"
-lastName = "Kim"
+firstName = "Hanni"
+lastName = "Pham"
+email = "hannipham04@gmail.com"
+age = 21
 
-print("My name is " + firstName + " " + lastName)
+print(f"My name is {lastName} {firstName}")
+print(f"I'm currently {age} years old.")
+print(f"\n[EMAIL] {email}")
